7bf56cea86ceac1bf18bd651d0cacbec4a1a5412
Jettchen <jettchen12345@gmail.com>
2020-03-01 15:10:40 +0800

    Update a simple webapp with no UI

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 """
     The web version of encrypto
 """
-from flask import Flask
+from flask import Flask,render_template,request
+from encryptMessage import Encryptor
 app = Flask(__name__)
+enc = Encryptor('data.json')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/encrypt',methods=["POST"])
+def encrypt():
+    msg = request.form['message']
+    encrypted_message = enc.encrypt(msg)
+    return encrypted_message
+
+@app.route('/decrypt',methods=["POST"])
+def decrypt():
+    msg = request.form['message'].encode()
+    decrypted_message = enc.decrypt(msg)
+    return decrypted_message
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- encryptMessage.py
+++ encryptMessage.py
@@ -1,33 +1,26 @@
 #Encrypt messages with AES 256
 from cryptography.fernet import Fernet
 import json
-import pprint
+# import pprint
 class Encryptor(object):
 	def __init__(self,fileName):
 		self.fileName = fileName
 	def encrypt(self,message):
 		key = Fernet.generate_key()
-		# print("type:{}".format(type(key)))
 		f = Fernet(key)	
 		encoded = message.encode()
 		encrypted = f.encrypt(encoded)
-		# print('lol')
 		hsh = self.generate_hash(encrypted)
-		# print('yay')
 		with open(self.fileName) as f:
 			data = json.load(f)
-		# print(data)
 		_strkey = key.decode()
 		data[hsh] = {
 			"key":_strkey
 		}
-		# pprint.pprint(data)
-		# print('horay!')
 		with open(self.fileName,'w') as f:
 			json.dump(data,f)
 		return encrypted
 	def decrypt(self,encryptedMessage,destroy = False):
-		# print(type(encryptedMessage))
 		with open(self.fileName) as f:
 			data = json.load(f)
 		hsh = self.generate_hash(encryptedMessage)
@@ -35,7 +28,6 @@ class Encryptor(object):
 		f = Fernet(key)
 		_msg = encryptedMessage
 		res = f.decrypt(_msg).decode()
-		# print(res)
 		return res
 	def generate_hash(self,encrypted):
 		return encrypted.decode()
