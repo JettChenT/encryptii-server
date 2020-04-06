6e00c7206c7df17b782516c3f58a382b1461c6f1
JettChenT <jettchen12345@gmail.com>
2020-04-06 19:44:53 +0800

    Much better user interface

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ enc = Encryptor()
 def home():
     return render_template('index.html')
 
-@app.route('/encrypt',methods=["POST"])
+@app.route('/encrypt',methods=["POST","GET"])
 def encrypt():
     msg = request.form['message']
     encrypted_message = enc.encrypt(msg)
     return render_template('dis_encrypted.html',msg=encrypted_message.decode())
 
-@app.route('/decrypt',methods=["POST"])
+@app.route('/decrypt',methods=["POST","GET"])
 def decrypt():
     msg = request.form['message'].encode()
     decrypted_message = enc.decrypt(msg)
@@ -25,4 +25,4 @@ def decrypt():
     return render_template('dis_decrypted.html',msg=decrypted_message)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
