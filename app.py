2b3f812f14a6afb985a43af2bfc0f0d46b56cdd5
Ryan Cao <47623453+ryanccn@users.noreply.github.com>
2020-06-06 12:23:32 +0800

    Add auto-redirect to new site

--- app.py
+++ app.py
@@ -1,33 +1,20 @@
 """
-    The web version of encrypto
+    The web version of encrypto (auto-redirect)
 """
-from flask import Flask,render_template,request,redirect
-from encryptMessage import Encryptor
+from flask import Flaskredirect
 app = Flask(__name__)
-enc = Encryptor()
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return redirect("https://encryptii.now.sh")
 
-@app.route('/encrypt',methods=["POST","GET"])
+@app.route('/encrypt')
 def encrypt():
-    if request.method == "POST":
-        msg = request.form['message']
-        encrypted_message = enc.encrypt(msg)
-        return render_template('dis_encrypted.html',msg=encrypted_message.decode())
-    else:
-        return redirect('/')        
+    return redirect("https://encryptii.now.sh/encrypt")       
 
-@app.route('/decrypt',methods=["POST","GET"])
+@app.route('/decrypt')
 def decrypt():
-    if request.method == "POST":
-        msg = request.form['message'].encode()
-        decrypted_message = enc.decrypt(msg,destroy=True)
-        if decrypted_message == -1:
-            return 'The message you seek is either never created or destroyed.'
-        return render_template('dis_decrypted.html',msg=decrypted_message)
-    else:
-        return redirect('/')
+    return redirect("https://encryptii.now.sh/encrypt")
+
 if __name__ == '__main__':
     app.run()
